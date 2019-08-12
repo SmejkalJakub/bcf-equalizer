d159abc10e0dc152b4fa1e9119f741a679cad9d1
Jakub Smejkal <smejkal.jakub99@gmail.com>
2019-08-12 20:06:04 +0200

    Update

--- backup/application.c
+++ backup/application.c
@@ -1,16 +1,11 @@
 #include <application.h>
-#include "time.h"
-#include "FFT.h"
+#include "Adafruit_ZeroFFT.h"
 
 #include "bc_adc_dma.h"
 #include "bc_adc.h"
 
+
 #define APPLICATION_TASK_ID 0
-
-#define LOG_OUT 0         //set output of FFT library to linear not logarithmical
-#define LIN_OUT 1
-#define FFT_N 256         //set to 256 point fft
-
 
 /*
 If no PLL is NOT enabled, then the core clock is 2 MHz
@@ -22,7 +17,6 @@ Autoreload:
 #define ADC_DMA_8KHZ_PRESCALER 1000
 #define ADC_DMA_8KHZ_AUTORELOAD 4
 
-
 //the signal in signal.h has 2048 samples. Set this to a value between 16 and 2048 inclusive.
 //this must be a power of 2
 #define DATA_SIZE 512
@@ -30,24 +24,16 @@ Autoreload:
 //the sample rate
 #define FS 8000
 
-uint8_t buffer[1024];
+uint8_t buffer[512];
+
+q15_t signal[512];
 
 int sampleCount;
 
 int freqBand[8];
 
-//bc_gfx_t *pgfx;
+bc_gfx_t *pgfx;
 
-bc_led_strip_t led_strip;
-static uint32_t _dma_buffer[144 * 4 * 2]; // count * type * 2
-const bc_led_strip_buffer_t _led_strip_buffer =
-        {
-                .type = BC_LED_STRIP_TYPE_RGBW,
-                .count = 144,
-                .buffer = _dma_buffer
-        };
-
-void generateRandomSignal();
 void fillTheDisplay();
 
 void byte_to_int16(uint8_t *src, int16_t *dst, size_t size)
@@ -60,8 +46,10 @@ void byte_to_int16(uint8_t *src, int16_t *dst, size_t size)
 
 void calculate_fft()
 {
-    int i = 2;
-    //ZeroFFT(signal, DATA_SIZE);
+    int i = 1;
+
+
+    ZeroFFT(signal, DATA_SIZE);
 
     for(int j = 0; j < 8; j++)
     {
@@ -70,18 +58,17 @@ void calculate_fft()
         sampleCount = (pow(2, j) * 2);
         for(; i <= sampleCount; i++)
         {
-            //bc_log_debug("%.1f Hz: %d", FFT_BIN(i, FS, DATA_SIZE), signal[i]);
+            bc_log_debug("%.1f Hz: %d", FFT_BIN(i, FS, DATA_SIZE), signal[i]);
             //bc_log_debug("cislo: %d", signal[i]);
-            //average += signal[i];
-            count++;;
+            average += signal[i];
+            count++;
         }
-        //bc_log_debug("----------");
+        bc_log_debug("----------");
         average /= count;
         freqBand[j] = (int)average;
         //bc_log_debug("průměr: %d", freqBand[j]);
     }
 }
-
 
 
 static void adc_dma_event_handler(bc_dma_channel_t channel, bc_dma_event_t event, void *event_param)
@@ -98,61 +85,32 @@ static void adc_dma_event_handler(bc_dma_channel_t channel, bc_dma_event_t event
     }
     else*/ if (event == BC_DMA_EVENT_DONE)
     {
-        //byte_to_int16(buffer, signal, sizeof(buffer));
+        byte_to_int16(buffer, signal, sizeof(buffer));
         calculate_fft();
         bc_scheduler_plan_now(APPLICATION_TASK_ID);
 
         bc_adc_dma_stop();
-        bc_adc_oversampling_set(BC_ADC_CHANNEL_A0, BC_ADC_OVERSAMPLING_16);
         bc_adc_dma_start(BC_ADC_CHANNEL_A0, ADC_DMA_8KHZ_PRESCALER, ADC_DMA_8KHZ_AUTORELOAD);
     }
 }
 
+
 void application_init(void)
 {
-
-     // Initialize power module
-    bc_module_power_init();
-
-    // Initialize led-strip on power module
-    bc_led_strip_init(&led_strip, bc_module_power_get_led_strip_driver(), &_led_strip_buffer);
-    for(int i = 0; i < 4; i++)
-    {
-        bc_led_strip_set_pixel_rgbw(&led_strip, i, 0, 255, 0, 0);
-    }
-    for(int i = 5; i < 9; i++)
-    {
-        bc_led_strip_set_pixel_rgbw(&led_strip, i, 255, 255, 0, 0);
-    }
-    for(int i = 9; i < 13; i++)
-    {
-        bc_led_strip_set_pixel_rgbw(&led_strip, i, 255, 69, 0, 0);
-    }
-    for(int i = 14; i < 18; i++)
-    {
-        bc_led_strip_set_pixel_rgbw(&led_strip, i, 255, 0, 0, 0);
-    }
-
-    bc_led_strip_write(&led_strip);
-    
-    //bc_log_init(BC_LOG_LEVEL_DUMP, BC_LOG_TIMESTAMP_ABS);
+    bc_log_init(BC_LOG_LEVEL_DUMP, BC_LOG_TIMESTAMP_ABS);
 
     bc_system_pll_enable();
 
     // Initialize logging
 
-    /*bc_module_lcd_init();
+    bc_module_lcd_init();
     pgfx = bc_module_lcd_get_gfx();
 
     bc_gfx_set_font(pgfx, &bc_font_ubuntu_13);
 
-    // Initialize LCD button right
-    static bc_button_t lcd_right;
-    bc_button_init_virtual(&lcd_right, BC_MODULE_LCD_BUTTON_RIGHT, bc_module_lcd_get_button_driver(), false);
-    bc_button_set_event_handler(&lcd_right, lcd_button_right_event_handler, NULL);*/
-
     bc_adc_dma_init(buffer, sizeof(buffer));
     bc_adc_dma_set_event_handler(adc_dma_event_handler, NULL);
+    //bc_adc_oversampling_set(BC_ADC_CHANNEL_A0, BC_ADC_OVERSAMPLING_8);
 
     // Start circular ADC sampling on P0/A0 to the buffer
     bc_adc_dma_start(BC_ADC_CHANNEL_A0, ADC_DMA_8KHZ_PRESCALER, ADC_DMA_8KHZ_AUTORELOAD);
@@ -166,14 +124,14 @@ void application_task(void)
 {
     int lastPos = 0;
 
-    /*if (!bc_gfx_display_is_ready(pgfx))
+    if (!bc_gfx_display_is_ready(pgfx))
     {
         bc_scheduler_plan_from_now(APPLICATION_TASK_ID, 2000);
         return;
-    }*/
+    }
 
     bc_system_pll_enable();
-    //bc_gfx_clear(pgfx);
+    bc_gfx_clear(pgfx);
 
     for(int j = 0; j < 8; j++)
     {
@@ -181,11 +139,13 @@ void application_task(void)
         {
             freqBand[j] = 127;
         }
-        //bc_gfx_draw_rectangle(pgfx, lastPos, 128, (lastPos+14), (128 - (freqBand[j])), true);
-        //bc_gfx_draw_rectangle(pgfx, lastPos, 128, (lastPos+14), (128 - (log_lookup[freqBand[j]])), true);
+        bc_gfx_draw_rectangle(pgfx, lastPos, 128, (lastPos+14), (128 - (log_lookup[freqBand[j]])), true);
         lastPos+=16;
     }
-    //bc_log_debug("konec");
-    //bc_gfx_update(pgfx);
+    bc_log_debug("konec");
+    bc_gfx_update(pgfx);
+    bc_system_pll_disable();
+
+    bc_scheduler_plan_current_relative(100);
 }
 
---
+++ backup/signal.h
@@ -0,0 +1,6 @@
+#ifndef _SIGNAL_H
+#define _SIGNAL_H
+
+#include "Adafruit_ZeroFFT.h"
+
+#endif
